1650d00577b1d65f194e72f2942c69ae4b362974
JDFleitas <marcelofolk17@gmail.com>
2023-11-24 01:11:47 -0300

    se trabajo con el Dom

--- topping-de-pizza/app.js
+++ topping-de-pizza/app.js
@@ -1,2 +1,12 @@
-const primerTopping = document.querySelector('.topping');
-console.log(primerTopping.style);
+const listaDeToppings = document.getElementById('lista-toppings');
+
+//Maneras de acceder al texto del nodo 
+
+// console.log('>innerText');
+// console.log(listaDeToppings.innerText);
+
+// console.log('>innerContent');
+// console.log(listaDeToppings.innerContent);
+
+// console.log('>innerHTML');
+// console.log(listaDeToppings.innerHTML);
